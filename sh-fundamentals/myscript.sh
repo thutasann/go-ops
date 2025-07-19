c30e64bd9f89a1453fbb9bebf352c911a1de8e12
thutasann <thutasann2002@gmail.com>
2025-07-19 17:11:53 +0800

    add(sh): var and user input

---
+++ sh-fundamentals/myscript.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# ECHO COMMAND
+# echo hello world!
+
+# VARIABLES
+# uppercase by convention
+# Letters, numbers, underscors
+# NAME="Brad"
+# echo "My name is ${NAME}"
+
+# USER INPUT
+read -p "Enter your name: " NAME
+echo "HELLO $NAME, nice to meet you"
