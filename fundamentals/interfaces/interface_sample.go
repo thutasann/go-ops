1916922bae7eb718db6f0dcfbb1e95b1f5c5a3c5
thutasann <thutasann2002@gmail.com>
2025-07-21 20:42:42 +0800

    test(fundamentals): interface methods

--- fundamentals/interfaces/interface_sample.go
+++ fundamentals/interfaces/interface_sample.go
@@ -7,19 +7,26 @@ import (
 
 type Player interface {
 	KickBall()
+	Name() string
 }
 
 type FootballPlayer struct {
+	name    string
 	stamina int
 	power   int
 }
 
 func (f FootballPlayer) KickBall() {
 	shot := f.stamina + f.power
-	fmt.Println("I'm kicking the ball: ", shot)
+	fmt.Printf("%s is kicking the ball:  %d\n", f.Name(), shot)
+}
+
+func (f FootballPlayer) Name() string {
+	return f.name
 }
 
 type CR7 struct {
+	name    string
 	stamina int
 	power   int
 	SUIII   int
@@ -30,17 +37,23 @@ func (f CR7) KickBall() {
 	fmt.Println("CR7's kicking the ball: ", shot)
 }
 
+func (f CR7) Name() string {
+	return f.name
+}
+
 func Interface_Sample() {
 	team := make([]Player, 11)
 
 	for i := 0; i < len(team)-1; i++ {
 		team[i] = FootballPlayer{
+			name:    fmt.Sprintf("random - %d", rand.Intn(2)),
 			stamina: rand.Intn(10),
 			power:   rand.Intn(10),
 		}
 	}
 
 	team[len(team)-1] = CR7{
+		name:    "Ronaldo",
 		stamina: 10,
 		power:   10,
 		SUIII:   1000000,
