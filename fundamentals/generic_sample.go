b4734d99009b267a0377acb7b8964387fa150bea
thutasann <thutasann2002@gmail.com>
2025-07-26 01:52:48 +0800

    add(fundamentals): generic filter sample

--- fundamentals/generic_sample.go
+++ fundamentals/generic_sample.go
@@ -10,6 +10,23 @@ func Generic_Sample() {
 	fmt.Printf("plusOne: %v\n", plusOne(t2))
 }
 
+func Generic_Sample_Two() {
+	ints := []int{1, 2, 3, 4}
+	evens := filter(ints, func(i int) bool { return i%2 == 0 })
+	fmt.Println(evens) // [2 4]
+}
+
 func plusOne[V int | float64 | int64 | float32 | int32](t V) V {
 	return t + 1
+}
+
+func filter[T any](s []T, f func(T) bool) []T {
+	var result []T
+	for _, v := range s {
+		if f(v) {
+			result = append(result, v)
+		}
+	}
+
+	return result
 }
--- go-docker/README.md
+++ go-docker/README.md
@@ -1,1 +1,17 @@
 # Simple Demo of Golang + Docker
+
+## Commands
+
+```bash
+docker build -t test-server .
+```
+
+```bash
+docker images |grep test-server
+```
+
+```bash
+docker run --rm -it -p 8080:8080 test-server
+
+curl localhost:8080
+```
