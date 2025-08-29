36d2752f1e1986a6bd83b300f7df6cf967f44652
thutasann <thutasann2002@gmail.com>
2025-08-29 18:47:28 +0800

    refactor: get lines from channel

--- tcp-http/main.go
+++ tcp-http/main.go
@@ -3,9 +3,44 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+
+	}()
+
+	return out
+}
 
 func main() {
 	f, err := os.Open("messages.txt")
@@ -13,25 +48,8 @@ func main() {
 		log.Fatal("error", "error", err)
 	}
 
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-		str += string(data)
-	}
-
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
