ccdeaa450defd4788f293fdae804add21a1ee2fc
thutasann <thutasann2002@gmail.com>
2025-08-29 17:24:05 +0800

    add: read file

--- tcp-http/main.go
+++ tcp-http/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+
+	str := ""
+	for {
+		data := make([]byte, 8)
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			str += string(data[:i])
+			data = data[i+1:]
+			fmt.Printf("read: %s\n", str)
+			str = ""
+		}
+		str += string(data)
+	}
+
+	if len(str) != 0 {
+		fmt.Printf("read: %s\n", str)
+	}
+}
